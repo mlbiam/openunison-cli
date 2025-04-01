83b9912cdac7ca59c0dcd2c32a5250d948329079
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2025-04-01 15:17:46 -0400

    use the right sha command

--- build.sh
+++ build.sh
@@ -42,9 +42,9 @@ rm -rf target/win
 
 
 
-export MACOS_SHA256=$(shasum -a 256 ./target/openunison-cli-$VERSION-macos.zip | awk '{print $1}')
-export LINUX_SHA256=$(shasum -a 256 ./target/openunison-cli-$VERSION-linux.zip | awk '{print $1}')
-export WIN_SHA256=$(shasum -a 256 ./target/openunison-cli-$VERSION-win.zip | awk '{print $1}')
+export MACOS_SHA256=$(sha256sum ./target/openunison-cli-$VERSION-macos.zip | awk '{print $1}')
+export LINUX_SHA256=$(sha256sum ./target/openunison-cli-$VERSION-linux.zip | awk '{print $1}')
+export WIN_SHA256=$(sha256sum ./target/openunison-cli-$VERSION-win.zip | awk '{print $1}')
 
 cat openunison-cli.yaml | sed "s/_VERSION_/$VERSION/g" | sed "s/_MAC_SHA_/$MACOS_SHA256/g" | sed "s/_LINUX_SHA_/$LINUX_SHA256/g" | sed "s/_WIN_SHA_/$WIN_SHA256/g" > target/openunison-cli.yaml
 
