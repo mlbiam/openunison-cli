4f00819f6ab88627b38054cddfb059f650bc0222
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2025-04-17 21:11:45 -0400

    set the expiration based on the token

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/gofrs/flock v0.12.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
--- pkg/kubecreds.go
+++ pkg/kubecreds.go
@@ -2,15 +2,22 @@ package outokens
 
 import (
 	"encoding/json"
+	"fmt"
 	"time"
 
+	"github.com/golang-jwt/jwt"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/pkg/apis/clientauthentication/v1beta1"
 )
 
 // GenerateExecCredential takes an ID token as a string and returns a populated ExecCredential object.
 func GenerateExecCredential(token string, forceBeta bool) (*v1beta1.ExecCredential, error) {
-	expirationTimestamp := time.Now().Add(5 * time.Minute) // Adjust as needed
+	expirationTimestamp, err := getExpirationTimestamp(token)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to get expiration timestamp: %w", err)
+	}
+	// If the token is expired, return an error
 
 	if forceBeta {
 		return &v1beta1.ExecCredential{
@@ -41,4 +48,41 @@ func GenerateExecCredential(token string, forceBeta bool) (*v1beta1.ExecCredenti
 // MarshalExecCredential returns the JSON encoding of the ExecCredential.
 func MarshalExecCredential(cred *v1beta1.ExecCredential) ([]byte, error) {
 	return json.MarshalIndent(cred, "", "  ")
+}
+
+// Gets the expiration timestamp from the token string.
+func getExpirationTimestamp(tokenString string) (time.Time, error) {
+	// We don't need to validate the token for this use case, just parse the claims
+	token, _, err := new(jwt.Parser).ParseUnverified(tokenString, jwt.MapClaims{})
+	if err != nil {
+		return time.Time{}, fmt.Errorf("failed to parse token: %w", err)
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return time.Time{}, fmt.Errorf("invalid claims")
+	}
+
+	expRaw, ok := claims["exp"]
+	if !ok {
+		return time.Time{}, fmt.Errorf("no exp claim in token")
+	}
+
+	// exp can be float64 or json.Number
+	var expUnix int64
+	switch v := expRaw.(type) {
+	case float64:
+		expUnix = int64(v)
+	case json.Number:
+		var err error
+		expUnix, err = v.Int64()
+		if err != nil {
+			return time.Time{}, fmt.Errorf("invalid exp format: %w", err)
+		}
+	default:
+		return time.Time{}, fmt.Errorf("unsupported exp type: %T", expRaw)
+	}
+
+	expTime := time.Unix(expUnix, 0).UTC()
+	return expTime, nil
 }
